9ccc2dd2b27d2ec1185fed8c07a2c01353c2185d
Kyle Walters <kyle.p.walters@gmail.com>
2020-07-28 20:13:03 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count do |string|
-    if class = String 
+    if string = String 
       puts count
   end
 end
